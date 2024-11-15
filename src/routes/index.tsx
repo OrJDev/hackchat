ef5860eb0956b89ffefc1c1d9fc877dbba8e28f8
OrJDev <orjdeveloper@gmail.com>
2024-11-15 03:53:07 +0200

    example video

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,7 +4,7 @@ import { Disclist } from "~/components";
 
 const Home: VoidComponent = () => {
   return (
-    <main class="flex flex-col gap-2 items-center justify-center">
+    <main class="flex flex-col gap-2 items-center justify-center pb-24">
       <h1 class="text-2xl sm:text-3xl font-bold text-offwhite">
         Welcome To{" "}
         <span class="decoration-offwhite underline decoration-dotted text-purple-500">
@@ -45,6 +45,26 @@ const Home: VoidComponent = () => {
           Prisma: "https://www.prisma.io",
         }}
       />
+      <div class="flex flex-col gap-2 items-center py-12">
+        <h1 class="font-bold text-offwhite text-2xl underline decoration-dotted decoration-purple-500">
+          Example
+        </h1>
+        <video
+          autoplay
+          loop
+          muted
+          playsinline
+          class="w-[80vw] sm:w-[60vw] rounded-lg"
+        >
+          <source
+            src="https://utfs.io/f/MUxXX3HTLdRjgiO1jR8VPXWpsUMAxmaJ6z4lB9b20H7rYtI3"
+            type="video/mp4"
+          />
+          <p class="text-xl font-bold text-white">
+            Your browser does not support the video tag.
+          </p>
+        </video>
+      </div>
     </main>
   );
 };
