733f65bfb626071f93bfcef7230a4ee5c71ffa74
OrJDev <orjdeveloper@gmail.com>
2024-11-15 16:49:17 +0200

    remove console log

--- src/routes/api/watchlist.ts
+++ src/routes/api/watchlist.ts
@@ -4,26 +4,30 @@ import { getServerSession } from "~/server/auth";
 import { pusher } from "~/server/pusher";
 
 export const POST: APIHandler = async (event) => {
-  console.log(await event.request.clone().text());
-  const body = await event.request.text();
-  const socketId = body?.split("socket_id=")[1];
+  try {
+    const body = await event.request.text();
+    const socketId = body?.split("socket_id=")[1];
 
-  if (!socketId) {
-    return json({ error: "Something went wrong" });
-  }
+    if (!socketId) {
+      return json({ error: "Something went wrong" });
+    }
 
-  const session = await getServerSession(event);
-  if (!session?.user) {
-    return json({ error: "Unknown User" });
+    const session = await getServerSession(event);
+    if (!session?.user) {
+      return json({ error: "Unknown User" });
+    }
+    console.log(
+      `The watchlist for ${session.user.name} is, ${session.user.contacts
+        .map((e) => e.user.name)
+        .join(", ")}`
+    );
+    const authResponse = pusher.authenticateUser(socketId, {
+      id: session.user.id,
+      watchlist: session.user.contacts.map((e) => e.user.id),
+    });
+    return json(authResponse);
+  } catch (e) {
+    console.error("@watchlist", e);
+    return json({ error: e });
   }
-  console.log(
-    `The watchlist for ${session.user.name} is, ${session.user.contacts
-      .map((e) => e.user.name)
-      .join(", ")}`
-  );
-  const authResponse = pusher.authenticateUser(socketId, {
-    id: session.user.id,
-    watchlist: session.user.contacts.map((e) => e.user.id),
-  });
-  return json(authResponse);
 };
