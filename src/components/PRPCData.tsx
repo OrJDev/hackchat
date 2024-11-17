080786b9774e3f5e2ee40b2230cb4bc81033a4e8
OrJDev <orjdeveloper@gmail.com>
2024-11-17 02:24:02 +0200

    latest

--- src/components/PRPCData.tsx
+++ src/components/PRPCData.tsx
@@ -9,29 +9,25 @@ export const RenderPRPCData = <T extends object>(props: {
 }) => {
   return (
     <Show
-      when={props.data || props.error?.()}
+      when={props.error?.()}
       fallback={<RenderChildren children={props.children} />}
     >
-      <>
-        <div class="font-bold text-red-500 text-xl bg-zinc-900 rounded-lg p-3 flex items-center justify-center my-12">
-          <p>
-            {props.data && "error" in props.data
-              ? props.data.error
-              : props.error?.()?.message
-              ? props.error?.()?.message
-              : "Something Went Wrong"}
-          </p>
-        </div>
-        <A
-          href="/dashboard"
-          class="disabled:animate-pulse hover:bg-zinc-700 disabled:bg-opacity-50 text-red-500 font-bold transition-all text-sm w-28 h-12 rounded-lg p-3 flex items-center justify-center"
-          style={{
-            "box-shadow": `0 0 0 1px #555`,
-          }}
-        >
-          Go Home
-        </A>
-      </>
+      {(err) => (
+        <>
+          <div class="font-bold text-red-500 text-xl bg-zinc-900 rounded-lg p-3 flex items-center justify-center my-12">
+            <p>{err().message ?? "Something Went Wrong"}</p>
+          </div>
+          <A
+            href="/dashboard"
+            class="disabled:animate-pulse hover:bg-zinc-700 disabled:bg-opacity-50 text-red-500 font-bold transition-all text-sm w-28 h-12 rounded-lg p-3 flex items-center justify-center"
+            style={{
+              "box-shadow": `0 0 0 1px #555`,
+            }}
+          >
+            Go Home
+          </A>
+        </>
+      )}
     </Show>
   );
 };
