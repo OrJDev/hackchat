3d906c9270708960c2bfc5f45fbd204a2ff2637c
OrJDev <orjdeveloper@gmail.com>
2024-11-17 02:28:42 +0200

    try that

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,4 +1,8 @@
 import { isServer } from "solid-js/web";
 
 export const getUrl = () =>
-  isServer ? process.env.AUTH_URL! : window.location.origin;
+  import.meta.env.VITE_SITE_URL
+    ? import.meta.env.VITE_SITE_URL
+    : isServer
+    ? process.env.AUTH_URL!
+    : window.location.origin;
